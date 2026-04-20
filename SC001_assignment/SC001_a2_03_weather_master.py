b49b4766eb661297e8c74bb54d9bf9d9c1b83942
Sanny Lin <150927857+sannylin325@users.noreply.github.com>
2026-04-20 10:18:32 +0800

    Update SC001_a2_03_weather_master.py

--- SC001_assignment/SC001_a2_03_weather_master.py
+++ SC001_assignment/SC001_a2_03_weather_master.py
@@ -5,8 +5,6 @@ Name: Sanny Lin
 This program should implement a console program
 that asks weather data from user to compute the
 average, highest, lowest, cold days among the inputs.
-Output format should match what is shown in the sample
-run in the Assignment 2 Handout.
 
 """
 # the number used to leave this program
