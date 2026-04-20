68229e45a09d66be7aff4c460ec05bafdff52af1
Sanny Lin <150927857+sannylin325@users.noreply.github.com>
2026-04-20 10:17:06 +0800

    Update SC001_a2_02_hailstone.py

--- SC001_assignment/SC001_a2_02_hailstone.py
+++ SC001_assignment/SC001_a2_02_hailstone.py
@@ -4,8 +4,7 @@ Name: Sanny Lin
 -----------------------
 This program should implement a console program that simulates
 the execution of the Hailstone sequence, defined by Douglas
-Hofstadter. Output format should match what is shown in the sample
-run in the Assignment 2 Handout.
+Hofstadter. 
 """
 
 
